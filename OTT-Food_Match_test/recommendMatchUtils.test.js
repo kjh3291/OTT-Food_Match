cf28d1969fa1caaa9d9bc2ffa80365fe128d9fbc
JongHyeok0706 <seohc91@naver.com>
2026-06-06 19:42:56 +0900

    유닛 테스트 코드 함수 추가

--- OTT-Food_Match_test/recommendMatchUtils.test.js
+++ OTT-Food_Match_test/recommendMatchUtils.test.js
@@ -21,16 +21,16 @@ describe('getGenreFromFoodCategory', () => {
 
 describe('getCategoriesForSituation', () => {
   test('장르 태그 매칭', () => {
-    expect(getCategoriesForSituation('액션', '혼밥')).toEqual(['치킨','패스트푸드','중식']);
+    expect(getCategoriesForSituation('액션', '혼밥')).toEqual(['치킨', '패스트푸드', '중식']);
   });
   test('전체 장르는 7개 카테고리 전부', () => {
     expect(getCategoriesForSituation('전체', '혼밥')).toEqual(
-      expect.arrayContaining(['한식','중식','일식','양식','패스트푸드','디저트','치킨'])
+      expect.arrayContaining(['한식', '중식', '일식', '양식', '패스트푸드', '디저트', '치킨'])
     );
   });
   test('식사 상황(야식) 태그가 합쳐지고 중복은 제거된다', () => {
     const r = getCategoriesForSituation('액션', '야식');
     expect(new Set(r).size).toBe(r.length);
-    expect(r).toEqual(expect.arrayContaining(['치킨','패스트푸드','중식']));
+    expect(r).toEqual(expect.arrayContaining(['치킨', '패스트푸드', '중식']));
   });
 });
