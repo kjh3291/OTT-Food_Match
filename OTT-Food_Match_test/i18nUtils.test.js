7176a22cd43f5c4b1a447522160e79c312535794
JongHyeok0706 <seohc91@naver.com>
2026-06-06 19:52:38 +0900

    유닛 테스트 추가: t 함수에 대한 테스트 케이스 작성

--- OTT-Food_Match_test/i18nUtils.test.js
+++ OTT-Food_Match_test/i18nUtils.test.js
@@ -11,4 +11,15 @@ describe('getLang', () => {
   });
 });
 
-
+describe('t', () => {
+  afterEach(() => localStorage.clear());
+  test('현재 언어에 맞는 번역을 반환', () => {
+    localStorage.setItem('lang', 'en');
+    expect(t('tab_action')).toBe('Action');
+    localStorage.setItem('lang', 'ja');
+    expect(t('tab_action')).toBe('アクション');
+  });
+  test('없는 키는 키 자체를 반환(폴백)', () => {
+    expect(t('존재하지않는키')).toBe('존재하지않는키');
+  });
+});
