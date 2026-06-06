a60e187d42168125d91dd6ad1098e68cc4886c1c
JongHyeok0706 <seohc91@naver.com>
2026-06-06 19:54:47 +0900

    유닛 테스트 추가: matchesCategory 함수에 대한 테스트 케이스 작성

--- OTT-Food_Match_test/mapUtils.test.js
+++ OTT-Food_Match_test/mapUtils.test.js
@@ -10,4 +10,22 @@ describe('getSearchConfig', () => {
   });
 });
 
-
+describe('matchesCategory', () => {
+  test('일식은 돈까스/초밥도 포함', () => {
+    expect(matchesCategory('일본식>돈까스', '일식')).toBe(true);
+    expect(matchesCategory('일식>초밥', '일식')).toBe(true);
+    expect(matchesCategory('한식>국밥', '일식')).toBe(false);
+  });
+  test('양식은 피자/파스타도 포함', () => {
+    expect(matchesCategory('음식점>피자', '양식')).toBe(true);
+  });
+  test('치킨은 통닭도 포함', () => {
+    expect(matchesCategory('치킨,통닭', '치킨')).toBe(true);
+  });
+  test('알 수 없는 키워드는 모두 통과', () => {
+    expect(matchesCategory('아무거나', '전체')).toBe(true);
+  });
+  test('category_name이 없어도 안전하게 동작', () => {
+    expect(matchesCategory(null, '한식')).toBe(false);
+  });
+});
